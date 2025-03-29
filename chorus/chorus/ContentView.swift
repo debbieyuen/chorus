a893b78bacd74d704fe0734886aa78ef3a8dd498
Debbie Yuen <deborahyuen2016@berkeley.edu>
2025-03-29 13:35:39 -0700

    initial project files

---
+++ chorus/chorus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chorus
+//
+//  Created by Debbie Yuen on 3/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Chorus")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chorus/chorus/chorusApp.swift
@@ -0,0 +1,17 @@
+//
+//  chorusApp.swift
+//  chorus
+//
+//  Created by Debbie Yuen on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct chorusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
